eacf3bd929a92883c69084e1816f6534f527e20e
LiorG <liorgish@gmail.com>
2017-11-19 17:00:48 +0200

    more

--- src/app/content-box/content-box.component.ts
+++ src/app/content-box/content-box.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {DataService} from "../services/data.service";
 
 @Component({
-  selector: 'app-content-box',
-  templateUrl: './content-box.component.html',
-  styleUrls: ['./content-box.component.scss'],
-  encapsulation: ViewEncapsulation.None
+    selector: 'app-content-box',
+    templateUrl: './content-box.component.html',
+    styleUrls: ['./content-box.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class ContentBoxComponent implements OnInit {
 
-  constructor(private dataService: DataService) { }
     content: string;
-  ngOnInit() {
-  }
+
+    constructor(private dataService: DataService) {
+    }
+
+    ngOnInit() {
+    }
 
     send() {
         this.dataService.sendMsg(this.content);
